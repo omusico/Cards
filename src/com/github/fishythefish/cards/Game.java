2b62faa66bd231b05765e417ec5495f8c9f52559
aokiroch <aokiroch@18.111.84.140>
2013-10-06 01:53:48 -0400

    a

--- src/com/github/fishythefish/cards/Game.java
+++ src/com/github/fishythefish/cards/Game.java
@@ -5,13 +5,13 @@ import com.firebase.client.Firebase;
 import com.firebase.client.ValueEventListener;
 
 public class Game {
-	public static String HOME = "https://cardsapp.firebaseIO.com/test/";
+	public static String HOME = "https://cardsapp.firebaseIO.com/";
 	
 	long game_num;
 	
 	FirebaseIO fb;
 	
-	public Game() {
+	public Game(String name) {
 		Firebase temp = new Firebase(HOME);
 		
 		temp.addValueEventListener(new ValueEventListener() {
@@ -25,9 +25,23 @@ public class Game {
 		});
 		
 		fb = new FirebaseIO(HOME + game_num);
+		//fb.setValue();
+		
+		//fb = new FirebaseIO(HOME + game_num + "/" + name);
+		fb.write(name, 0);
+	}
+	
+	public Game(String number, String name) {
+		game_num = Integer.parseInt(number);
+		fb = new FirebaseIO(HOME + game_num);
+		fb.write(name, 0);
 	}
 	
 	public long getGameNum() {
 		return game_num;
+	}
+	
+	public FirebaseIO getFIO() {
+		return fb;
 	}
 }
